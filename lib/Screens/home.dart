f551d431fea5542bd06513172729296306d3960e
William samson <99078154+WilliamSamson@users.noreply.github.com>
2022-08-25 13:46:19 +0100

    Update home.dart

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -3,17 +3,29 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
-  static const String _title = 'Flutter Code Sample';
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 24, 94, 28),
+        elevation: 20,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(30),
+          ),
+        ),
+        centerTitle: true,
         title: const Text(
           'Menu',
           style: TextStyle(
             fontSize: 20,
+            fontWeight: FontWeight.bold,
           ),
+        ),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.only(
+          top: 16.0,
         ),
       ),
     );
